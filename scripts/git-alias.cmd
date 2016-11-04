df914fb3e73b695cc9fdc19d0b9563f5de0043bf
Amadeusz Wieczorek <git@amadeusw.com>
2016-11-03 17:54:24 -0700

    Stores my git aliases

---
+++ scripts/git-alias.cmd
@@ -0,0 +1,6 @@
+git alias.st status --short
+git alias.last log --stat -1 HEAD
+git alias.ls log --stat --pretty=short --graph
+git alias.put !git add .; git commit -m
+git alias.please push --force-with-lease
+git alias.amend commit --amend --no-edit
