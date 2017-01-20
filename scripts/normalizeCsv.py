40a5397a7017b083e93e8fc01c0fb8735188f207
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-20 00:58:29 -0800

    csv wip: scan through file

--- scripts/normalizeCsv.py
+++ scripts/normalizeCsv.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import re
 
 parser = argparse.ArgumentParser(description="Clean up units in CSV file.")
 parser.add_argument('filename', metavar='filename', help='file to process.')
@@ -7,10 +8,22 @@ parser.add_argument('column', metavar='column', help='column to process.')
 parser.add_argument('unit', metavar='unit', nargs='?', help='target unit. Leave blank for most popular unit.')
 
 args = parser.parse_args()
+unitHistogram = {}
 
-with open (filename, 'w') as csvfile:
-    reader = csv.reader(csvfile)
-    headers = reader.next()
-    column = headers.index(args.column)
-    print(column)
-    
+with open (args.filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        target = row[args.column]
+        if target == None:
+            raise ValueError()
+        print(target)
+
+        matches = re.search("(\d*(\.\d+)?)((\D)*)", target)
+        value = matches.group(1)
+        unit = matches.group(3)
+        print(value)
+        print(unit)
+        unitHistogram.setdefault(unit, 0)
+        unitHistogram[unit] = unitHistogram[unit] + 1
+
+print(unitHistogram)
