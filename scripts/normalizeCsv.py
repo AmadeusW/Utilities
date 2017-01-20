eb1ca170e96be7e44cebd00de8822022e115eae4
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-20 08:19:57 -0800

    prettify

--- scripts/normalizeCsv.py
+++ scripts/normalizeCsv.py
@@ -14,24 +14,29 @@ parser.add_argument('column', metavar='column',
                     help='column to process.')
 parser.add_argument('unit', metavar='unit', nargs='?',
                     help='target unit. Leave blank for most popular unit.')
-
 args = parser.parse_args()
-unitHistogram = {}
+
+TIME = { 's': 1, 'ms': 1000, 'us': 1000*1000, 'ns': 1000*1000*1000 }
+SIZE = { 'B': 1, 'kB': 1024, 'MB': 1024*1024, 'GB': 1024*1024*1024 }
 
 with open(args.filename, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        target = row[args.column]
-        if target is None:
-            raise ValueError()
-        print(target)
 
-        matches = re.search(r"(\d*(\.\d+)?)((\D)*)", target)
-        value = matches.group(1)
-        unit = matches.group(3)
-        print(value)
-        print(unit)
-        unitHistogram.setdefault(unit, 0)
-        unitHistogram[unit] = unitHistogram[unit] + 1
+    unit = args.unit
+    # First pass: determine which unit is the most common
+    if unit is None:
+        unitHistogram = {}
+        for row in reader:
+            target = row[args.column]
+            if target is None:
+                raise ValueError()
+            matches = re.search(r"(\d*(\.\d+)?)((\D)*)", target)
+            currentUnit = matches.group(3)
+            unitHistogram.setdefault(currentUnit, 0)
+            unitHistogram[currentUnit] = unitHistogram[currentUnit] + 1
 
-print(unitHistogram)
+        unit = max(unitHistogram, key=unitHistogram.get)
+        print('Determined target conversion unit: ' + unit)
+    else:
+        print('Using provided conversion unit: ' + unit)
+    # Convert values to the target unit
