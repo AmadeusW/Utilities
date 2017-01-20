120a0ac3a20ad0033889b57daafebb2c540e4d58
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-20 08:42:21 -0800

    conversion!

--- scripts/normalizeCsv.py
+++ scripts/normalizeCsv.py
@@ -16,21 +16,24 @@ parser.add_argument('unit', metavar='unit', nargs='?',
                     help='target unit. Leave blank for most popular unit.')
 args = parser.parse_args()
 
-TIME = { 's': 1, 'ms': 1000, 'us': 1000*1000, 'ns': 1000*1000*1000 }
-SIZE = { 'B': 1, 'kB': 1024, 'MB': 1024*1024, 'GB': 1024*1024*1024 }
+FACTORS = { 
+    's': 1, 'ms': 1000, 'us': 1000*1000, 'ns': 1000*1000*1000,
+    'B': 1, 'kB': 1024, 'MB': 1024*1024, 'GB': 1024*1024*1024
+}
 
 with open(args.filename, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
-
+    rows = list(reader)
+    print(rows)
     unit = args.unit
     # First pass: determine which unit is the most common
     if unit is None:
         unitHistogram = {}
-        for row in reader:
+        for row in rows:
             target = row[args.column]
             if target is None:
                 raise ValueError()
-            matches = re.search(r"(\d*(\.\d+)?)((\D)*)", target)
+            matches = re.search(r"(\d*(\.\d+)?)\s*((\D)*)", target)
             currentUnit = matches.group(3)
             unitHistogram.setdefault(currentUnit, 0)
             unitHistogram[currentUnit] = unitHistogram[currentUnit] + 1
@@ -40,3 +43,16 @@ with open(args.filename, 'r') as csvfile:
     else:
         print('Using provided conversion unit: ' + unit)
     # Convert values to the target unit
+    for row in rows:
+        target = row[args.column]
+        if target is None:
+            raise ValueError()
+        matches = re.search(r"(\d*(\.\d+)?)\s*((\D)*)", target)
+        currentUnit = matches.group(3)
+        if (currentUnit == unit):
+            print('No conversion necessary for ' + matches.group(1) + '[' + matches.group(3) + ']')
+        else:
+            print('Converting ' + matches.group(1) + '[' + matches.group(3) + '] into [' + unit + ']' )
+        factor = FACTORS[unit] / FACTORS[currentUnit]
+        newValue = float(matches.group(1)) * factor
+        print('New value is ' + str(newValue) + '[' + unit + ']')
