687673af3089195616c0256fe6672a3bbcc858a8
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-20 07:59:17 -0800

    Addresses some pylint comments

--- scripts/normalizeCsv.py
+++ scripts/normalizeCsv.py
@@ -1,11 +1,16 @@
+"""Script originally written to fix  """
+
 import argparse
 import csv
 import re
 
 parser = argparse.ArgumentParser(description="Clean up units in CSV file.")
-parser.add_argument('filename', metavar='filename', help='file to process.')
-parser.add_argument('column', metavar='column', help='column to process.')
-parser.add_argument('unit', metavar='unit', nargs='?', help='target unit. Leave blank for most popular unit.')
+parser.add_argument('filename', metavar='filename',
+                    help='file to process.')
+parser.add_argument('column', metavar='column',
+                    help='column to process.')
+parser.add_argument('unit', metavar='unit', nargs='?',
+                    help='target unit. Leave blank for most popular unit.')
 
 args = parser.parse_args()
 unitHistogram = {}
@@ -18,7 +23,7 @@ with open (args.filename, 'r') as csvfile:
             raise ValueError()
         print(target)
 
-        matches = re.search("(\d*(\.\d+)?)((\D)*)", target)
+        matches = re.search(r"(\d*(\.\d+)?)((\D)*)", target)
         value = matches.group(1)
         unit = matches.group(3)
         print(value)
