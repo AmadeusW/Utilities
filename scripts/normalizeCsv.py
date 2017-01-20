d2574aaaa77d82fd0045f4baec80062665f9216e
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-19 17:03:04 -0800

    begin work on Normalize CSV script

---
+++ scripts/normalizeCsv.py
@@ -0,0 +1,12 @@
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description="Clean up units in CSV file.")
+parser.add_argument('filename', metavar='filename', help='file to process.')
+parser.add_argument('column', metavar='column', help='column to process.')
+parser.add_argument('unit', metavar='unit', nargs='?', help='target unit. Leave blank for most popular unit.')
+
+args = parser.parse_args()
+
+#with open (filename, 'rb') as csvfile:
+    
