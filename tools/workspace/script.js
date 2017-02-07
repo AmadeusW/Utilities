ddd16db8ba34eb9dc1abab3f6998477374a7dc25
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-06 18:07:32 -0800

    creates new elements

--- tools/workspace/script.js
+++ tools/workspace/script.js
@@ -1,3 +1,23 @@
-  $( function() {
-    $("section").draggable({ cursor: "move", handle: ".handle", snap: "true", scroll: true, scrollSensitivity: 100 });
-  } );
+var state = {};
+
+$( function() {
+
+    $("#add").click(function(event) {
+        var newItem = {
+            title: "New window",
+            comment: "",
+            code: "",
+            state: 0,
+        }
+        var section = $("#template").clone().attr("id", "newid").appendTo("#main");
+        section.draggable({ 
+            cursor: "move", 
+            handle: ".handle", 
+            snap: "true", 
+            scroll: true, 
+            scrollSensitivity: 100 }
+        );
+        state.push(newItem);
+    });
+
+});
