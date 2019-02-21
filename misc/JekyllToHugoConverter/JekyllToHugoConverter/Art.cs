40a6989bb65e7ebc908311be5f701f303e189b4f
Amadeusz Wieczorek <git@amadeusw.com>
2019-02-21 09:13:31 -0800

    adds JekyllToHugoConverter

---
+++ misc/JekyllToHugoConverter/JekyllToHugoConverter/Art.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JekyllToHugoConverter
+{
+    class Art
+    {
+        public string Internal { get; }
+        public string Title { get; set; }= string.Empty;
+        public string Description { get; set; }= string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Tags { get; set; } = string.Empty;
+
+        public string ToMd()
+        {
+            return $@"+++
+image = ""{Url}""
+title = ""{Title}""
+weight = 0
+description = ""{Description}""
+availability = """"
+tags = [{Tags}]
++++
+";
+        }
+
+        public Art(string @internal)
+        {
+            Internal = @internal;
+        }
+
+        public void AddTag(string tag)
+        {
+            if (string.IsNullOrEmpty(tag))
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(Tags))
+            {
+                Tags = $"\"{tag}\"";
+                return;
+            }
+            else
+            {
+                Tags += $", \"{tag}\"";
+                return;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Internal.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Art otherArt))
+                return false;
+
+            return Internal.Equals(otherArt.Internal);
+        }
+    }
+}
---
+++ misc/JekyllToHugoConverter/JekyllToHugoConverter/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JekyllToHugoConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.EnumerateFiles(@"C:\src\elamalara.github.io\jekyll\_data\categories", "*.yml");
+            Dictionary<string, Art> pieces = new Dictionary<string, Art>();
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Reading {file}");
+                LoadData(file, pieces);
+            }
+            foreach (var piece in pieces.Values)
+            {
+                Console.WriteLine($"Saving {piece.Title}");
+                SaveData(@"C:\src\elamalara.github.io\content\art", piece);
+            }
+        }
+
+        private static void LoadData(string file, Dictionary<string, Art> pieces)
+        {
+            Art current = null;
+            string tag = string.Empty;
+
+            foreach (var line in File.ReadAllLines(file))
+            {
+                if (line.StartsWith("-"))
+                {
+                    var key = line.Split(':')[1].Trim();
+                    if (pieces.TryGetValue(key, out var existingPiece))
+                    {
+                        current = existingPiece;
+                        current.AddTag(tag);
+                        Console.WriteLine($"Updating {key}");
+                    }
+                    else
+                    {
+                        current = new Art(key);
+                        current.AddTag(tag);
+                        pieces.Add(key, current);
+                        Console.WriteLine($"Processing {key}");
+                    }
+                }
+                if (current == null)
+                {
+                    if (line.StartsWith("title:"))
+                    {
+                        tag = line.Split(':')[1].Trim();
+                    }
+                    continue; // we are in front matter, there is no point continuing
+                }
+
+                var parts = line.Split(':');
+                if (parts.Length == 1)
+                    continue; // we did not split, don't continue
+
+                var property = parts[0];
+                var value = parts[1].Trim();
+                switch (property.TrimStart('-', ' ', '\t'))
+                {
+                    case "internal":
+                        break;
+                    case "title":
+                        current.Title = value.Trim('"').Replace("\"", "\\\"");
+                        break;
+                    case "description":
+                        var newValue = value.Trim('"').Replace("\"", "\\\"");
+                        if (newValue == "Please write a brief description.")
+                            break;
+                        current.Description = newValue;
+                        break;
+                    case "url":
+                        current.Url = value.Trim('"');
+                        break;
+                }
+            }
+        }
+
+        private static void SaveData(string directoryPath, Art piece)
+        {
+            Directory.CreateDirectory(directoryPath);
+            File.WriteAllText(Path.Combine(directoryPath, piece.Internal + ".md"), piece.ToMd());
+        }
+    }
+}
